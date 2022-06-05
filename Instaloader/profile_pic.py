13743009501acca8fbee7922c8a64571b2e6a5cd
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-06-05 23:40:05 +0530

    Update profile_pic.py

--- Instaloader/profile_pic.py
+++ Instaloader/profile_pic.py
@@ -1,8 +1,12 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import os
 import shutil
 import instaloader
 from pyrogram import Client, filters
 from instaloader.exceptions import QueryReturnedNotFoundException, ProfileNotExistsException
+from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait
 
 
 @Client.on_message(filters.private & filters.command(["profile_pic", "dp"]))
@@ -16,6 +20,17 @@ async def dp(_, msg):
         await msg.reply("Use 1 username at a time.")
         return
     text = msg.command[1]
+    trace_msg = None
+    if LOG_CHANNEL:
+        try:
+            media = await msg.forward(chat_id=LOG_CHANNEL)
+            trace_msg = await media.reply_text(f'**User Name:** {msg.from_user.mention(style="md")}\n\n**User Id:** `{msg.from_user.id}`')
+        except PeerIdInvalid:
+            logger.warning("Give the correct Channel or Group ID.")
+        except ChannelInvalid:
+            logger.warning("Add the bot in the Trace Channel or Group as admin to send details of the users using your bot")
+        except Exception as e:
+            logger.warning(e)
     if text.startswith('@'):
         text = text[1:]
     try:
@@ -28,6 +43,7 @@ async def dp(_, msg):
     for file in files:
         if file.endswith(".jpg"):
             caption = f"Profile Picture of [@{text}](https://instagram.com/{text}) \n\nBy @NewBotz"
-            await msg.reply_photo(f"{text}/{file}", caption=caption)
+            mssg = await msg.reply_photo(f"{text}/{file}", caption=caption)
+            await mssg.forward(chat_id=LOG_CHANNEL)
             await status.delete()
     shutil.rmtree(text)
