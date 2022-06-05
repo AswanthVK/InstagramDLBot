8a437a3959dee29a6e36b0ad370e6c99476e32d2
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-06-05 22:21:30 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -13,7 +13,7 @@ You can also authorize me to download private posts.
 
 Use below buttons to learn more.
 
-By @StarkBots
+By @NewBotz
     """
 
     # Home Button
@@ -23,12 +23,12 @@ By @StarkBots
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
+        #[InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/NewBotz")],
     ]
 
     # Help Message
@@ -52,13 +52,13 @@ Use /auth to authorize and /unauth to unauthorize.
     ABOUT = """
 **About This Bot** 
 
-A telegram bot to download instagram content by @StarkBots
+A telegram bot to download instagram content by @NewBotz
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/InstagramBot)
+#Source Code : [Click Here](https://github.com/StarkBotsIndustries/InstagramBot)
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
 
-Developer : @StarkProgrammer
+Developer : @AswanthVK
     """
