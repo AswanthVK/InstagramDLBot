f1330d097abfd28859217af14d95140e00e177d9
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-07-02 10:53:34 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -25,7 +25,7 @@ else:
     BOT_TOKEN = ""
     DATABASE_URL = ""
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = "NewBotz"
+    MUST_JOIN = ""
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
     INSTA_USERNAME = ""
